e6e85b8a22fa4d0b39a77ca6943b8e70f894288d
Mattiwos <44147357+Mattiwos@users.noreply.github.com>
2019-05-11 03:02:43 -0700

    Update sketch.js

--- Data Tranfer/public/sketch.js
+++ Data Tranfer/public/sketch.js
@@ -18,16 +18,16 @@ textSize(10)
 text("Arduino 1 results:",0,height/2)
 text(distancead[0],0,height/2+50)
 //Arduino2
-text("Arduino 2 results:",50,height/2)
-text(distancead[1],50,height/2+50)
+text("Arduino 2 results:",100,height/2)
+text(distancead[1],100,height/2+50)
 //Arduino3
-text("Arduino 3 results:",100,height/2)
-text(distancead[2],100,height/2+50)
+text("Arduino 3 results:",200,height/2)
+text(distancead[2],200,height/2+50)
 //Arduino4
-text("Arduino 4 results:",150,height/2)
-text(distancead[3],150,height/2+50)
+text("Arduino 4 results:",300,height/2)
+text(distancead[3],300,height/2+50)
 //Arduino5
-text("Arduino 5 results:",200,height/2)
-text(distancead[4],200,height/2+50)
+text("Arduino 5 results:",400,height/2)
+text(distancead[4],400,height/2+50)
 
 }
